4d9816cda455794f18fd76565f243a4079891d17
Matheus <matheuskauadecarvalho@gmail.com>
2026-07-06 19:35:48 -0300

    Atualizações no codigo na parte do sistema de confirmar para excluir post

--- script.js
+++ script.js
@@ -278,13 +278,13 @@ listaPosts.addEventListener("click", function(event){
         
         else{
             let resposta = confirm("Tem certeza que deseja excluir este post?")
-            if (resposta === true){
+            if (resposta){
         posts = posts.filter(function(post){
         return post.id !== Number(id)
         })
         localStorage.setItem("posts", JSON.stringify(posts))
         mostrarPosts()
-            }else{return}
+            }
         }
     }
 
